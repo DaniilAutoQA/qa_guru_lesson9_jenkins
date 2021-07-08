1d044e37691f12bfae4afe1fd491ff1c7f657683
DaniilAutoQA <daniil-bond@yandex.ru>
2021-07-08 14:58:54 +0300

    add priperties

---
+++ src/test/java/test/SystemPropertiesTests.java
@@ -0,0 +1,38 @@
+package test;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTests {
+
+    @Test
+    void setAndReadFromProperties(){
+        System.setProperty("our properties", "communizm");
+        String value = System.getProperty("our properties", "democracy");
+        System.out.println(value);
+        //communizm
+    }
+
+    @Test
+    void readDefaultFromProperties(){
+        String value = System.getProperty("our properties", "democracy");
+        System.out.println(value);
+        //democracy
+    }
+
+    @Test
+    void readGradleFromProperties(){
+        System.getProperty("browser", "chrome");
+        System.out.println(System.getProperty("browser"));
+        //opera
+    }
+
+    @Test
+    @Tag("terminal")
+    void readTerminalFromProperties(){
+        System.out.println(System.getProperty("browser","FF"));
+        System.out.println(System.getProperty("version", "91"));
+    }
+
+}
